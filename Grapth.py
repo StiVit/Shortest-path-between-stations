e23e1bc3f3a17e3a98cba3d09cbc3c2b16e9c5aa
vitalie stinca <vitalicstinca123@gmail.com>
2023-10-27 02:29:29 +0100

    Implemented Task1, Task2 using libraries

---
+++ Grapth.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import networkx as nx
+
+class UndergroundMap():
+    def __init__(self):
+        self.graph = nx.Graph()
+
+    def create_data_base(self, path):
+        self.data_set = pd.read_excel(path)
+        for index, row in self.data_set.iterrows():
+            station1 = row[1]
+            station2 = row[2]
+            weight = row[3]
+            self.graph.add_edge(station1, station2, weight=weight)
+
+    def get_graph(self):
+        return self.graph
+    
+map = UndergroundMap()
+map.create_data_base('London Underground data.xlsx')
+graph = map.get_graph()
+    
+
+shortest_path = nx.shortest_path_length(graph, source='Holborn', target='Mile End', weight='weight')
+print("Shortest travel time:", shortest_path)
+shortest_path = nx.shortest_path(graph, source='Holborn', target='Mile End', weight='weight')
+print("Shortest path:", shortest_path)
