e3fe7f4695ef434763cf1cbf3501faee8c8bf962
tudoriustin123 <145880960+tudoriustin123@users.noreply.github.com>
2023-11-02 22:32:53 +0000

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,5 +1,6 @@
 from single_source_shortest_paths import initialize_single_source
 from Grapth import UndergroundMap
+import matplotlib.pyplot as plt
 
 def dijkstra(G, start_station, end_station):
     # Initialize distances and predecessors
@@ -56,7 +57,7 @@ if __name__ == "__main__":
     london_underground_graph = map.get_graph()
     stations = map.get_station_indexes()
 
-    start_station_name = 'Stratford'
+    start_station_name = 'Holborn'
     end_station_name = 'Moor Park'
 
     # Find the shortest duration between the start and end stations
@@ -65,3 +66,18 @@ if __name__ == "__main__":
 
     # Display the shortest duration
     print(f"Shortest Duration: {shortest_duration} minutes")
+
+    # Calculate and plot the histogram of journey times between station pairs
+    all_durations = []
+    for start_station in stations.values():
+        for end_station in stations.values():
+            duration = dijkstra(london_underground_graph, start_station, end_station)
+            all_durations.append(duration)
+
+    plt.figure(figsize=(8, 6))
+    plt.hist(all_durations, bins=20, color='skyblue')
+    plt.xlabel('Journey Durations (minutes)')
+    plt.ylabel('Frequency')
+    plt.title('Distribution of Journey Times between Station Pairs')
+    plt.grid(True)
+    plt.show()
