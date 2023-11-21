ee1ae8ebc519e87902c49254b210eeee42a6a29c
Tuong-Luan Bach <bachxtuongluan@gmail.com>
2023-11-21 18:30:09 +0000

    Changed code to be more pythonic. Recreated Histogram

--- Task3/Task3a-Bellmon_Ford.py
+++ Task3/Task3a-Bellmon_Ford.py
@@ -1,6 +1,7 @@
 from bellman_ford import bellman_ford
 import time
 import pandas as pd
+# import matplotlib.pyplot as plt
 from adjacency_list_graph import AdjacencyListGraph
 
 
@@ -41,7 +42,7 @@ class UndergroundMap:
 
             # check if the edge between two stations already exists, if not, create
             if not self.graph.has_edge(indexes[station1], indexes[station2]):
-                self.graph.insert_edge(indexes[station1], indexes[station2], weight=self.weight)
+                self.graph.insert_edge(indexes[station1], indexes[station2], weight=weight)
         self.stations = indexes
 
     def get_graph(self):
@@ -57,26 +58,25 @@ def get_user_input():
     return user_start_station, user_end_station
 
 
-def get_number_of_stations():
-    list_of_previous = get_list_previous()
+def get_number_of_stations(n_start, n_end):
+    list_of_previous = get_list_previous(n_start, n_end)
     return len(list_of_previous)
 
 
-def get_list_previous():
+def get_list_previous(f_start, f_end):
     # This part is to trace back on itself to find the number of stations between the start and end stations
     # It basically uses the predecessors returned by the bellman ford algorithm.
     # it starts by getting the position of the end station's predecessor
     # it then uses a do while loop to get the predecessor of the next station.
     # This keeps looping until the start station is reached.
-    new_pos = pi[pos_of_station[end_station]]
+    new_pos = pi[pos_of_station[f_end]]
     list_of_previous = []
-
+    # list out keys and values separately
+    key_list = list(pos_of_station.keys())
+    val_list = list(pos_of_station.values())
     while True:  # using a do while loop
 
-        if new_pos != pos_of_station[start_station]:  # check if the trace back as reached the starting station
-            # list out keys and values separately
-            key_list = list(pos_of_station.keys())
-            val_list = list(pos_of_station.values())
+        if new_pos != pos_of_station[f_start]:  # check if the trace back as reached the starting station
 
             position = val_list.index(new_pos)
             list_of_previous.append((key_list[position]))  # create a list of previous stations
@@ -93,19 +93,37 @@ if __name__ == "__main__":
 
     start = time.time()
 
-    map = UndergroundMap()  # Creating an instance of the UndergroundMap class from Graph.py
-    map.create_data_base('London Underground data.xlsx')  # Load data into the graph from the provided Excel file
-    london_underground_graph = map.get_graph()  # Get the graph representing the London Underground
-    pos_of_station = map.get_station_indexes()  # get the dict of stations positions within the index
-    graph = map.get_graph()
+    underground_map = UndergroundMap()  # Creating an instance of the UndergroundMap class from Graph.py
+    underground_map.create_data_base('London Underground data.xlsx')  # Load data into the graph from the provided
+    # Excel file
+    london_underground_graph = underground_map.get_graph()  # Get the graph representing the London Underground
+    pos_of_station = underground_map.get_station_indexes()  # get the dict of stations positions within the index
+    graph = underground_map.get_graph()
 
     d, pi, cycle = bellman_ford(london_underground_graph, pos_of_station[start_station])  # use the bellman ford
     # search algorithm to find the shortest path to every single station
 
-    number_stops = get_number_of_stations()
-    list_of_stations_to_end = get_list_previous()
+    user_number_stops = get_number_of_stations(start_station, end_station)
+    list_of_stations_to_end = get_list_previous(start_station, end_station)
     print(f'The stations between {start_station} and {end_station} is {list_of_stations_to_end}')
-    print(f'Number of stops between {start_station} and {end_station} is {number_stops} ')
+    print(f'Number of stops between {start_station} and {end_station} is {user_number_stops} ')
+
+    # Create histogram here
+    # stops = []
+    # for l_start_station in pos_of_station:
+    #     for l_end_station in pos_of_station:
+    #         if l_end_station != l_start_station:
+    #             d, pi, cycle = bellman_ford(graph, pos_of_station[l_start_station])
+    #             number_of_stops = get_number_of_stations(l_start_station, l_end_station)
+    #             stops.append(number_of_stops)
+    #
+    # plt.figure(figsize=(8, 6))
+    # plt.hist(stops, bins=20, color='skyblue')
+    # plt.xlabel('Number of Stations')
+    # plt.ylabel('Frequency')
+    # plt.title('Distribution of number of stops between Station Pairs')
+    # plt.grid(True)
+    # plt.show()
 
     end = time.time()
     print(f'The time taken is {end - start}')
