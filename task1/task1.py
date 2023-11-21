c6eda05ff23c2b3488719533855394be03fa9ed8
vitalie stinca <vitalicstinca123@gmail.com>
2023-11-21 17:54:21 +0000

    task 1-3 fix

--- task1/task1.py
+++ task1/task1.py
@@ -8,14 +8,14 @@ if __name__ == "__main__":
     # Create the UndergroundMap and read the station data
     map = UndergroundMap()
     map.create_data_base('../London Underground data.xlsx')
-    grapth = map.get_graph()
+    graph = map.get_graph()
     stations = map.get_station_indexes()
 
     start_station_name = input('Introduce the stationA: ').strip()
     end_station_name = input('Introduce the stationB: ').strip()
 
     # Find the shortest duration between the start and end stations
-    shortest_durations, pi = dijkstra(grapth, stations[start_station_name])
+    shortest_durations, pi = dijkstra(graph, stations[start_station_name])
 
     # Display the shortest duration
     print(f"Shortest Duration: {shortest_durations[stations[end_station_name]]} minutes")
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     # Calculate and plot the histogram of journey times between station pairs
     all_durations = []
     for start_station in stations.values():
-        durations, pi = dijkstra(grapth, start_station)
+        durations, pi = dijkstra(graph, start_station)
         for duration in durations:
             all_durations.append(duration)
 
