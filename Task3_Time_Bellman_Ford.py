8c7eb3eb133109a6890e761aa8b28a6c5f6248cb
N0vaR00t <129861810+N0vaR00t@users.noreply.github.com>
2023-11-26 19:20:53 +0000

    Update Task3_Time_Bellman_Ford.py

--- Task3_Time_Bellman_Ford.py
+++ Task3_Time_Bellman_Ford.py
@@ -1,11 +1,12 @@
 """
 Diana Nykonenko
 Task 3 - the minimum time taken
-Using Bellman Ford
+Using Bellman-Ford
 """
 
-from bellman_ford import bellman_ford                                          # importing the class and the functiong from mst.py
+from bellman_ford import bellman_ford                                          # importing the class and the function from mst.py
 from Graph import UndergroundMap                                               # importing the class UndergroundMap from Graph.py
+import time                                                                    # for efficiency tracking
 
 
 if __name__ == "__main__":                                                     # testing whether sscript is being run directly
@@ -16,8 +17,8 @@ if __name__ == "__main__":                                                     #
     tube_graph = tube_map.get_graph()                                          # getting the graph of the Tube Map
     stations = tube_map.get_station_indexes()                                  # getting indexes
 
-    start_station = 'Holborn'                                                  # the starting point for the algorithm
-    end_station = 'Highgate'                                                   # the ending point for the alghorithm
+    start_station = 'Paddington'                                               # the starting point for the algorithm
+    end_station = 'Waterloo'                                                   # the ending point for the algorithm
     end_index = int(stations.get(end_station))                                 # storing the index of the start station
     d, pi, cycle = bellman_ford(tube_graph, stations[start_station])           # calling the algorithm 
 
@@ -30,6 +31,9 @@ if __name__ == "__main__":                                                     #
     print(d, pi, cycle)                                                       
     """
 
-
+    start = time.time()                                                        # record time point
     min_time = int(d[end_index])                                               # finding the minimum time
+    end = time.time()                                                          # record time point
+    timer = end - start                                                        # getting the final time
     print("The mininmum travel time is", min_time, "minutes.")                 # printing the answer
+    print("Time taken by the algorithm - ", timer)                             # total time
